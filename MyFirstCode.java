acafde319a84c14a4d4a92ab9246ad43c57d722f
madurchand <41477354+madurchand@users.noreply.github.com>
2018-07-24 17:30:36 +1000

    Add files via upload

---
+++ MyFirstCode.java
@@ -0,0 +1,12 @@
+
+public class MyFirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20, b = 30, c;
+		c = a + b ;
+		System.out.println("Result:" +c);
+		System.out.println("Hello World I have successfully created my first java program");
+	}
+
+}
